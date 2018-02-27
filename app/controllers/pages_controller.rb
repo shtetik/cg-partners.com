38b1cd67d471b5b078b080b56675cebce168df9e
Pavel Kulikov <shtetik@gmail.com>
2018-02-27 16:31:23 +0700

    [new] static pages and routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def startup
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root to: 'pages#home'
+
+  PagesController.action_methods.each do |action|
+    get "/#{action}", to: "pages##{action}", as: "#{action}"
+  end
 end
