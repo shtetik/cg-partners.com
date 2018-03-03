1f5c9a231b4ba8b1043e2ca245b370c835ed0eca
Pavel Kulikov <shtetik@gmail.com>
2018-03-04 00:44:05 +0700

    [fix] js check Trestle presence

--- app/assets/javascripts/trestle/custom/tinymce.js
+++ app/assets/javascripts/trestle/custom/tinymce.js
@@ -1,11 +1,13 @@
-Trestle.TinyMCE = {};
+if (typeof Trestle !== 'undefined') {
+  Trestle.TinyMCE = {};
 
-Trestle.init(function(e, root) {
-  $(root).find('textarea.tinymce').each(function() {
-    var id = $(this).attr('id');
-    var editor = tinyMCE.get(id);
-    if (editor) { editor.remove(); }
+  Trestle.init(function(e, root) {
+    $(root).find('textarea.tinymce').each(function() {
+      var id = $(this).attr('id');
+      var editor = tinyMCE.get(id);
+      if (editor) { editor.remove(); }
+    });
+
+    tinyMCE.init(Trestle.TinyMCE.default);
   });
-
-  tinyMCE.init(Trestle.TinyMCE.default);
-});
+}
