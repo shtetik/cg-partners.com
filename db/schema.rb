c48d3931967dd1c85edc7eebcdd90fe001e8855c
Pavel Kulikov <shtetik@gmail.com>
2018-03-03 18:33:33 +0700

    [update] schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303040348) do
+ActiveRecord::Schema.define(version: 20180303104316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,4 +26,81 @@ ActiveRecord::Schema.define(version: 20180303040348) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.string "locale"
+    t.index ["locale"], name: "index_friendly_id_slugs_on_locale"
+    t.index ["slug", "sluggable_type", "locale"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_locale"
+    t.index ["slug", "sluggable_type", "scope", "locale"], name: "index_friendly_id_slugs_uniqueness", unique: true
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.text "emails", default: [], array: true
+    t.string "position", default: "", null: false
+    t.string "telephone", default: "", null: false
+    t.string "fax", default: "", null: false
+    t.string "vat", default: "", null: false
+    t.string "linkedin", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
+  create_table "person_translations", force: :cascade do |t|
+    t.integer "person_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "full_name", null: false
+    t.text "text", null: false
+    t.string "slug", null: false
+    t.index ["locale"], name: "index_person_translations_on_locale"
+    t.index ["person_id"], name: "index_person_translations_on_person_id"
+  end
+
+  create_table "service_translations", force: :cascade do |t|
+    t.integer "service_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title", null: false
+    t.text "text", null: false
+    t.string "slug", null: false
+    t.index ["locale"], name: "index_service_translations_on_locale"
+    t.index ["service_id"], name: "index_service_translations_on_service_id"
+  end
+
+  create_table "service_type_translations", force: :cascade do |t|
+    t.integer "service_type_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "slug", null: false
+    t.index ["locale"], name: "index_service_type_translations_on_locale"
+    t.index ["service_type_id"], name: "index_service_type_translations_on_service_type_id"
+  end
+
+  create_table "service_types", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "service_type_id", null: false
+    t.index ["service_type_id"], name: "index_services_on_service_type_id"
+  end
+
+  add_foreign_key "services", "service_types"
 end
