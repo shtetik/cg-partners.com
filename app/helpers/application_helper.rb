a70f34a0d5f4e54f56617e9db9066784274a58e5
Pavel Kulikov <shtetik@gmail.com>
2018-03-04 01:54:54 +0700

    [new] locale change helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-  def locale_path(locale = I18n.default_locale)
+  def other_locale
+    (I18n.available_locales - [locale]).shift
+  end
+
+  def locale_path(locale = other_locale)
     locales = I18n.available_locales.join('|')
     path = request.fullpath.sub!(/^\/(?:#{locales})(\/|$)/, "/#{locale}#{'\1'}")
     path || root_path(locale: locale)
+  end
+
+  def object_locale_path(object, locale = other_locale)
+    url_for id: object.send(:"slug_#{other_locale}"), locale: locale
   end
 end
