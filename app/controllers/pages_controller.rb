1c6c26175720d19ccadee2180d872a38b65b8ccd
Pavel Kulikov <shtetik@gmail.com>
2018-03-07 01:16:09 +0700

    [update] actual people and services on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
-  def home;end
+  def home
+    @people = Person.includes(:translations).order(:id)
+    @services = Service
+      .includes({ service_type: :translations }, :translations)
+      .order(:id)
+    @services_group_by_type = @services.group_by(&:service_type)
+  end
 
   def startup;end
 
